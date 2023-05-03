947978c64910351230ea595e363051cd89fcb37d
delfigica <delfinamariacanas@gmail.com>
2023-05-03 20:05:00 -0300

    Loading feedback cardmovie

---
+++ src/components/Skeleton/SkeletonCard.tsx
@@ -0,0 +1,19 @@
+import { Skeleton, useTheme, useMediaQuery } from "@mui/material";
+
+export const SkeletonCard = () => {
+  const theme = useTheme();
+  const laptop = useMediaQuery(theme.breakpoints.up("lg"));
+
+  return (
+    <Skeleton
+      variant="rectangular"
+      width={300}
+      height={500}
+      sx={
+        laptop
+          ? { backgroundColor: "#3CCE88", margin: "15px" }
+          : { backgroundColor: "#3CCE88", margin: "15px 5px" }
+      }
+    />
+  );
+};
