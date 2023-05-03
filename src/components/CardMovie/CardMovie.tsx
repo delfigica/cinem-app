1a87685d97cc86b25659e17d8fcadfaf2ffe043f
delfigica <delfinamariacanas@gmail.com>
2023-05-03 20:04:44 -0300

    CardMovie creation

--- src/components/CardMovie/CardMovie.tsx
+++ src/components/CardMovie/CardMovie.tsx
@@ -1,50 +1,90 @@
 import {
   Box,
-  Typography,
   Card,
   CardMedia,
-  CardActionArea,
   Button,
   IconButton,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import Link from "next/link";
 
 export const CardMovie = ({ data }: any) => {
-  console.log("data cardMovie: ", data);
+  const theme = useTheme();
+
+  const laptop = useMediaQuery(theme.breakpoints.up("lg"));
+
   return (
-    <Card sx={{ width: "330px", height: "500px" }}>
+    <Card
+      sx={
+        laptop
+          ? { width: "330px", height: "500px", margin: "15px" }
+          : { widht: "250px", height: "550px", margin: "10px" }
+      }
+    >
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-          background: "rgba(223, 223, 223, 0.38)",
-          width: "330px",
-          height: "500px",
-          position: "absolute",
-          cursor: "pointer",
-        //   visibility: "collapse",
-        //   "&:hover": {
-        //       visibility: "visible",
-        //   },
-        }}
+        sx={
+          laptop
+            ? {
+                display: "flex",
+                justifyContent: "space-evenly",
+                alignItems: "center",
+                background: "rgba(223, 223, 223, 0.38)",
+                width: "330px",
+                height: "500px",
+                position: "absolute",
+                cursor: "pointer",
+                //   visibility: "collapse",
+                //   "&:hover": {
+                //       visibility: "visible",
+                //   },
+              }
+            : {
+                display: "none",
+              }
+        }
       >
         <IconButton>
-          <FavoriteIcon color="primary" sx={{ fontSize: "1.2em" }} />
+          <FavoriteBorderIcon color="secondary" sx={{ fontSize: "1.2em" }} />
         </IconButton>
-        <Button
-          variant="contained"
-          color="secondary"
-          sx={{ fontWeight: "600" }}
-        >
-          Ver detalle
-        </Button>
+        <Link href={"/movies/" + data.id}>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ fontWeight: "600" }}
+          >
+            Ver detalle
+          </Button>
+        </Link>
       </Box>
       <CardMedia
         image={"https://image.tmdb.org/t/p/w500" + data.poster_path}
-        sx={{ backgroundSize: "cover", width: "100%", height: "500px" }}
+        sx={{ backgroundSize: "contained", width: "100%", height: "500px" }}
         component="img"
       />
+      {!laptop && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            height: "50px",
+          }}
+        >
+          <IconButton>
+            <FavoriteBorderIcon color="secondary" sx={{ fontSize: "1.2em" }} />
+          </IconButton>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ fontWeight: "600" }}
+          >
+            Ver detalle
+          </Button>
+        </Box>
+      )}
     </Card>
   );
 };
