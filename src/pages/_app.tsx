956ec15a105e2ede14e071fbf2757fcbe427bcc3
delfigica <delfinamariacanas@gmail.com>
2023-05-04 21:26:54 -0300

    update head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import { store } from "../store";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
 import theme from "../theme.js";
 import "../styles/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Cocos Movies</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
