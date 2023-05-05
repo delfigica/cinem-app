2142c762c745e85db68fbc5da90220524f74029c
delfigica <delfinamariacanas@gmail.com>
2023-05-04 21:26:41 -0300

    update style

--- src/components/CardMovie/CardMovie.tsx
+++ src/components/CardMovie/CardMovie.tsx
@@ -34,95 +34,25 @@ export const CardMovie = ({ data }: any) => {
   };
 
   return (
-    <Card
-      sx={
-        laptop
-          ? { width: "280px", height: "440px", margin: "15px" }
-          : { widht: "250px", height: "550px", margin: "10px" }
-      }
-    >
-      <Box
+    <Link href={"/movies/" + data.id}>
+      <Card
         sx={
           laptop
             ? {
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignItems: "center",
-                background: "rgba(223, 223, 223, 0.38)",
                 width: "280px",
                 height: "440px",
-                position: "absolute",
+                margin: "15px",
                 cursor: "pointer",
-                //   visibility: "collapse",
-                //   "&:hover": {
-                //       visibility: "visible",
-                //   },
               }
-            : {
-                display: "none",
-              }
+            : { widht: "250px", height: "550px", margin: "10px" }
         }
       >
-        <IconButton
-          onClick={() => {
-            handlerFavorites(data.id);
-          }}
-        >
-          {wishListState.movies_ids.includes(data.id) ? (
-            <FavoriteIcon color="secondary" sx={{ fontSize: "1.2em" }} />
-          ) : (
-            <FavoriteBorderIcon color="secondary" sx={{ fontSize: "1.2em" }} />
-          )}
-        </IconButton>
-        <Link href={"/movies/" + data.id}>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ fontWeight: "600" }}
-          >
-            Ver detalle
-          </Button>
-        </Link>
-      </Box>
-      <CardMedia
-        image={"https://image.tmdb.org/t/p/w500" + data.poster_path}
-        sx={{ backgroundSize: "contained", width: "100%", height: "500px" }}
-        component="img"
-      />
-      {!laptop && (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            height: "50px",
-          }}
-        >
-          <IconButton
-            onClick={() => {
-              handlerFavorites(data.id);
-            }}
-          >
-            {wishListState.movies_ids.includes(data.id) ? (
-              <FavoriteIcon color="secondary" sx={{ fontSize: "1.2em" }} />
-            ) : (
-              <FavoriteBorderIcon
-                color="secondary"
-                sx={{ fontSize: "1.2em" }}
-              />
-            )}
-          </IconButton>
-          <Link href={"/movies/" + data.id}>
-            <Button
-              variant="contained"
-              color="primary"
-              sx={{ fontWeight: "600" }}
-            >
-              Ver detalle
-            </Button>
-          </Link>
-        </Box>
-      )}
-    </Card>
+        <CardMedia
+          image={"https://image.tmdb.org/t/p/w500" + data.poster_path}
+          sx={{ backgroundSize: "contained", width: "100%", height: "500px" }}
+          component="img"
+        />
+      </Card>
+    </Link>
   );
 };
